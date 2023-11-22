cbdd93d3f812498fbc9bbc2c6ada876fe1db2141
Monisha199 <63121362+Monisha199@users.noreply.github.com>
2023-11-22 22:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use('/',require('./routes'));
 
-app.listen(port, function(err){
+app.listen(port,'0.0.0.0', function(err){
     if(err){
         console.log('Error: ',err);
         console.log(`Error: ${err}`);
